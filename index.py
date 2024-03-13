e79e98911f7c5083b584102b560dacaa341fee11
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-03-13 00:35:54 -0500

    feat: added routes for calculating data with different input

--- index.py
+++ index.py
@@ -9,19 +9,49 @@ app = FastAPI(title="Pandemic Backend", description="Disease transmition model u
 
 
 @app.get("/", response_class=HTMLResponse, tags=["Infected and Susceptible"])
-async def calculate_data():
+async def calculate_default_data():
     beta = lambda t: 0.0005 if t <=10 else 0.0001
 
-    sir = SIR(0.1, beta, 1500, 1, 0)
+    sir = SIR(0.01, beta, 800, 1, 0)
     solver = ForwardEuler(sir)
     solver.set_initial_conditions(sir.initial_conditions)
 
-    time_steps = np.linspace(0, 60, 1001)
+    time_steps = np.linspace(0, 60, 60)
     u, t = solver.solve(time_steps)
+    return JSONResponse({"time": t.tolist(),"population": u.tolist()})
 
-    # plt.plot(t, u[:, 0], label="Susceptible")
-    # plt.plot(t, u[:, 1], label="Infected")
-    # plt.plot(t, u[:, 2], label="Recovered")
-    # plt.legend()
-    # plt.show()
+@app.get("/withconstants", response_class=HTMLResponse, tags=["Infected and Susceptible"])
+
+async def calculate_data_with_different_constant_values(nu: float =0.01, beta: float= 0.0003):
+
+    sir = SIR(nu, beta, 800, 1, 0)
+    solver = ForwardEuler(sir)
+    solver.set_initial_conditions(sir.initial_conditions)
+
+    time_steps = np.linspace(0, 60, 60)
+    u, t = solver.solve(time_steps)
+    return JSONResponse({"time": t.tolist(),"population": u.tolist()})
+
+@app.get("/initialvalues", response_class=HTMLResponse, tags=["Infected and Susceptible"])
+async def calculate_data_with_different_inital_values(susceptible: int= 800, infected: int = 1, recovered: int = 0):
+    
+    beta = lambda t: 0.0005 if t <=10 else 0.0001
+    sir = SIR(0.01, beta, susceptible, infected, recovered)
+    solver = ForwardEuler(sir)
+    solver.set_initial_conditions(sir.initial_conditions)
+
+    time_steps = np.linspace(0, 60, 60)
+    u, t = solver.solve(time_steps)
+    return JSONResponse({"time": t.tolist(),"population": u.tolist()})
+
+@app.get("/withoutrecovered", response_class=HTMLResponse, tags=["Infected and Susceptible"])
+async def calculate_data_without_recovered():
+    
+    beta = lambda t: 0.0005 if t <=10 else 0.0001
+    sir = SIR(0, beta, 800, 1, 0)
+    solver = ForwardEuler(sir)
+    solver.set_initial_conditions(sir.initial_conditions)
+
+    time_steps = np.linspace(0, 60, 60)
+    u, t = solver.solve(time_steps)
     return JSONResponse({"time": t.tolist(),"population": u.tolist()})
